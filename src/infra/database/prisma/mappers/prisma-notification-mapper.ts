86fd659a601119d522c042efc70bac28f58261a1
Weliton da Silva <welitondasilva00@gmail.com>
2023-05-07 01:17:01 -0300

    Chore: Final lesson. Application finished

---
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -0,0 +1,32 @@
+import { Notification as RawNotification } from '@prisma/client';
+import { Notification } from '@application/entities/notification';
+import { Content } from '@application/entities/content';
+
+export class PrismaNotificationMapper {
+  // Métodos estáticos não precisam ser instanciados
+  static toPrisma(notification: Notification) {
+    return {
+      id: notification.id,
+      category: notification.category,
+      content: notification.content.value,
+      recipientId: notification.recipientId,
+      readAt: notification.readAt,
+      canceledAt: notification.canceledAt,
+      createdAt: notification.createdAt,
+    };
+  }
+
+  static toDomain(raw: RawNotification) {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createdAt,
+      },
+      raw.id,
+    );
+  }
+}
